66ec2fc0e1b5b20b9b5dd8009d2d24249286732a
priyanshuPthak <priyanshupathak5808@gmail.com>
2026-04-04 21:06:11 +0530

    signup route

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import  Flask
-from db import init_db
+from flask import  Flask, request, redirect
+
+from db import init_db, get_connection
+
 
 app = Flask(__name__)
   
@@ -10,6 +12,37 @@ def home():
     return " Running successfully"
 
 
+
+@app.route("/signup", methods=["GET", "POST"])
+
+def  signup():
+    if request.method =="POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute(
+                "INSERT INTO users (username, password) VALUES (?, ?)",
+                (username, password)
+            )
+            conn.commit()
+            return "registered successfully "
+        except:
+            return "Username already exists"
+    
+    return """
+        <h2>Signup</h2>
+        <form method="post">
+            <input name="username" placeholder="Enter username"><br><br>
+            <input name="password" type="password" placeholder="Enter password"><br><br>
+            <button type="submit">Signup</button>
+        </form>
+    """    
+
 if __name__ == '__main__':
     app.run(debug =True)
+
 
