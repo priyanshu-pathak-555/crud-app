2e0d3d9deb0638ee9cf46905d297fc58bfa70034
priyanshuPthak <priyanshupathak5808@gmail.com>
2026-04-04 20:54:21 +0530

    created users db

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import  Flask
+from db import init_db
 
 app = Flask(__name__)
+  
+init_db()
 
 @app.route("/")
 def home():
---
+++ db.py
@@ -0,0 +1,25 @@
+import  sqlite3
+
+database_NAME = "users.db"
+
+
+def get_connection():
+    conn = sqlite3.connect(database_NAME)
+    return conn
+
+
+def init_db():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+    
